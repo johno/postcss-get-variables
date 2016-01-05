d319fd496094647201fa2b25b6b6f20f8e79d415
John Otander <johnotander@gmail.com>
2016-01-04 21:38:59 -0700

    Use ava, is-css-root, and css-variable

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 'use strict'
 
 var postcss = require('postcss')
+var isCssRoot = require('is-css-root')
+var cssVariable = require('css-variable')
 
-module.exports = postcss.plugin('get-variables', function(callback) {
-  return function(root, postcssResult) {
+module.exports = postcss.plugin('get-variables', function (callback) {
+  return function (root, postcssResult) {
     var variables = {}
 
-    root.eachRule(function(rule) {
-      if (rule.selectors.length === 1 && rule.selectors[0] === ':root' && rule.parent.type === 'root') {
-        rule.each(function(declaration) {
+    root.eachRule(function (rule) {
+      if (rule.selectors.length === 1 && isCssRoot(rule.selectors[0]) && rule.parent.type === 'root') {
+        rule.each(function (declaration) {
           if (declaration.type === 'decl' && declaration.prop) {
-            variables[declaration.prop.replace('--', '')] = declaration.value
+            var variable = cssVariable(declaration.prop)
+            variables[variable.base()] = declaration.value
           }
         })
       }
--- test/test.js
+++ test/test.js
@@ -1,29 +1,26 @@
 'use strict'
 
-var fs = require('fs')
-var assert = require('assert')
-var postcss = require('postcss')
-var isPresent = require('is-present')
-var getVariables = require('..')
+import fs from 'fs'
+import test from 'ava'
+import postcss from 'postcss'
+import isPresent from 'is-present'
+import getVariables from '..'
 
-describe('postcss-get-variables', function() {
 
-  it('should do something awesome', function(done) {
-    postcss()
-      .use(getVariables(function(variables) {
-        assert.ok(isPresent(variables))
-        assert.ok(isPresent(variables['primary-color']))
-      }))
-      .process(fixture('basic'))
-      .then(function() {
-        done()
-      })
-      .catch(function(error) {
-        done(error)
-      }) 
-  })
+test('postcss-get-variables', t => {
+  t.plan(2)
+
+  postcss()
+    .use(getVariables(variables => {
+      t.true(isPresent(variables))
+      t.true(isPresent(variables['primary-color']))
+    }))
+    .process(fixture('basic'))
+    .catch(error => {
+      console.log(error)
+    }) 
 })
 
-function fixture(name) {
-  return fs.readFileSync('test/fixtures/' + name + '.css', 'utf8').toString()
+function fixture (name) {
+  return fs.readFileSync('fixtures/' + name + '.css', 'utf8').toString()
 }
